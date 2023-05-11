de4ba51aee9d1ebcf747ee2d451f08c28b69bad2
Roberto Alencar <rroberto.alencar@gmail.com>
2023-05-11 15:38:45 -0300

    Enviando arquivos de entregador

---
+++ src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
@@ -0,0 +1,48 @@
+package br.com.ifpe.oxefood.api.entregador;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ifpe.oxefood.modelo.entregador.Entregador;
+import br.com.ifpe.oxefood.modelo.entregador.EntregadorService;
+import br.com.ifpe.oxefood.util.entity.GenericController;
+
+@RestController
+@RequestMapping("/api/entregador")
+public class EntregadorController extends GenericController {
+    
+    @Autowired
+    private EntregadorService entregadorService;
+
+    @PostMapping
+    public ResponseEntity<Entregador> save(@RequestBody @Valid EntregadorRequest request) {
+
+        Entregador entregadorNovo = request.build();
+        Entregador entregador = entregadorService.save(entregadorNovo);
+        return new ResponseEntity<Entregador>(entregador, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<Entregador> listarTodos() {
+  
+       return entregadorService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Entregador obterPorID(@PathVariable Long id) {
+
+       return entregadorService.obterPorID(id);
+    }
+
+}
---
+++ src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
@@ -0,0 +1,34 @@
+package br.com.ifpe.oxefood.modelo.entregador;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.ifpe.oxefood.util.entity.GenericService;
+
+@Service
+public class EntregadorService extends GenericService {
+    
+    @Autowired
+    private EntregadorRepository repository;
+
+    @Transactional
+    public Entregador save(Entregador entregador) {
+
+        super.preencherCamposAuditoria(entregador);
+        return repository.save(entregador);
+    }
+
+    public List<Entregador> listarTodos() {
+  
+        return repository.findAll();
+    }
+ 
+    public Entregador obterPorID(Long id) {
+ 
+        return repository.findById(id).get();
+    }
+}
